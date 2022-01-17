b01ef560269f797b1bf22e297997aa7514f1393a
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2022-01-17 17:53:32 +0000

    Start moving to subTests -- FunctionalTestCase

--- docutils/test/test_functional.py
+++ docutils/test/test_functional.py
@@ -17,6 +17,8 @@ import os.path
 import shutil
 import unittest
 import difflib
+from pathlib import Path
+
 from test import DocutilsTestSupport              # must be imported before docutils
 import docutils
 import docutils.core
@@ -68,115 +70,84 @@ expected output and check it in:
   svn add %(exp)s
   svn commit -m "<comment>" %(exp)s"""
 
-    def __init__(self, *args, configfile=None, **kwargs):
-        """
-        Set self.configfile, pass remaining arguments to parent.
-
-        Requires keyword argument `configfile`.
-
-        Note: the modified signature is incompatible with
-        the "pytest" and "nose" frameworks.
-        """ # cf. feature-request #81
-
-        assert configfile is not None, 'required argument'
-        self.configfile = configfile
-        super().__init__(*args, **kwargs)
-
-    def shortDescription(self):
-        return 'test_functional.py: ' + self.configfile
+    def setUp(self):
+        clear_output_directory()
 
     def test(self):
-        """Process self.configfile."""
-        # Keyword parameters for publish_file:
-        namespace = {}
-        # Initialize 'settings_overrides' for test settings scripts,
-        # and disable configuration files:
-        namespace['settings_overrides'] = {'_disable_config': True}
-        # Read the variables set in the default config file and in
-        # the current config file into namespace:
-        with open(join_path(datadir, 'tests', '_default.py')) as f:
-            defaultpy = f.read()
-            exec(defaultpy, namespace)
-        with open(self.configfile) as f:
-            exec(f.read(), namespace)
-        # Check for required settings:
-        assert 'test_source' in namespace,\
-               "No 'test_source' supplied in " + self.configfile
-        assert 'test_destination' in namespace,\
-               "No 'test_destination' supplied in " + self.configfile
-        # Set source_path and destination_path if not given:
-        namespace.setdefault('source_path',
-                             join_path(datadir, 'input',
-                                       namespace['test_source']))
-        # Path for actual output:
-        namespace.setdefault('destination_path',
-                             join_path(datadir, 'output',
-                                       namespace['test_destination']))
-        # Path for expected output:
-        expected_path = join_path(datadir, 'expected',
-                                  namespace['test_destination'])
-        # shallow copy of namespace to minimize:
-        params = namespace.copy()
-        # remove unneeded parameters:
-        del params['test_source']
-        del params['test_destination']
-        # Delete private stuff like params['__builtins__']:
-        for key in list(params.keys()):
-            if key.startswith('_'):
-                del params[key]
-        # Get output (automatically written to the output/ directory
-        # by publish_file):
-        output = docutils.core.publish_file(**params)
-        # ensure output is unicode
-        output_encoding = params.get('output_encoding', 'utf-8')
-        # Normalize line endings:
-        output = '\n'.join(output.splitlines())
-        # Get the expected output *after* writing the actual output.
-        no_expected = self.no_expected_template % {
-            'exp': expected_path, 'out': params['destination_path']}
-        self.assertTrue(os.access(expected_path, os.R_OK), no_expected)
-        # samples are UTF8 encoded. 'rb' leads to errors with Python 3!
-        f = open(expected_path, 'r', encoding='utf-8')
-        # Normalize line endings:
-        expected = '\n'.join(f.read().splitlines())
-        f.close()
-
-        diff = self.expected_output_differs_template % {
-            'exp': expected_path, 'out': params['destination_path']}
-        try:
-            self.assertEqual(output, expected, diff)
-        except AssertionError:
-            diff = ''.join(difflib.unified_diff(
-                expected.splitlines(True), output.splitlines(True),
-                expected_path, params['destination_path']))
-            print('\n%s:' % (self,), file=sys.stderr)
-            print(diff, file=sys.stderr)
-            raise
-        # Execute optional function containing extra tests:
-        if '_test_more' in namespace:
-            namespace['_test_more'](join_path(datadir, 'expected'),
-                                    join_path(datadir, 'output'),
-                                    self, namespace)
-
-
-def suite():
-    s = unittest.TestSuite()
-    clear_output_directory()
-    for root, dirs, files in os.walk(join_path(datadir, 'tests')):
-        # Process all config files among `names` in `dirname`. A config
-        # file is a Python file (*.py) which sets several variables.
-        for name in files:
-            if name.endswith('.py') and not name.startswith('_'):
-                config_file_full_path = join_path(root, name)
-                s.addTest(
-                    FunctionalTestCase("test",
-                                       input=None, expected=None,
-                                       id=config_file_full_path,
-                                       suite_settings={},
-                                       configfile=config_file_full_path)
-                )
-    assert list(s) != [], 'No functional tests found.'
-    return s
+        """Process configfile."""
+        for path in Path(datadir, 'tests').rglob("[!_]*.py"):
+            configfile = path.as_posix()
+            with self.subTest(id=configfile):
+                # Keyword parameters for publish_file:
+                namespace = {}
+                # Initialize 'settings_overrides' for test settings scripts,
+                # and disable configuration files:
+                namespace['settings_overrides'] = {'_disable_config': True}
+                # Read the variables set in the default config file and in
+                # the current config file into namespace:
+                with open(join_path(datadir, 'tests', '_default.py')) as f:
+                    defaultpy = f.read()
+                    exec(defaultpy, namespace)
+                with open(configfile) as f:
+                    exec(f.read(), namespace)
+                # Check for required settings:
+                assert 'test_source' in namespace,\
+                       "No 'test_source' supplied in " + configfile
+                assert 'test_destination' in namespace,\
+                       "No 'test_destination' supplied in " + configfile
+                # Set source_path and destination_path if not given:
+                namespace.setdefault('source_path',
+                                     join_path(datadir, 'input',
+                                               namespace['test_source']))
+                # Path for actual output:
+                namespace.setdefault('destination_path',
+                                     join_path(datadir, 'output',
+                                               namespace['test_destination']))
+                # Path for expected output:
+                expected_path = join_path(datadir, 'expected',
+                                          namespace['test_destination'])
+                # shallow copy of namespace to minimize:
+                params = namespace.copy()
+                # remove unneeded parameters:
+                del params['test_source']
+                del params['test_destination']
+                # Delete private stuff like params['__builtins__']:
+                for key in list(params.keys()):
+                    if key.startswith('_'):
+                        del params[key]
+                # Get output (automatically written to the output/ directory
+                # by publish_file):
+                output = docutils.core.publish_file(**params)
+                # ensure output is unicode
+                output_encoding = params.get('output_encoding', 'utf-8')
+                # Normalize line endings:
+                output = '\n'.join(output.splitlines())
+                # Get the expected output *after* writing the actual output.
+                no_expected = self.no_expected_template % {
+                    'exp': expected_path, 'out': params['destination_path']}
+                self.assertTrue(os.access(expected_path, os.R_OK), no_expected)
+                # samples are UTF8 encoded. 'rb' leads to errors with Python 3!
+                f = open(expected_path, 'r', encoding='utf-8')
+                # Normalize line endings:
+                expected = '\n'.join(f.read().splitlines())
+                f.close()
+        
+                diff = self.expected_output_differs_template % {
+                    'exp': expected_path, 'out': params['destination_path']}
+                try:
+                    self.assertEqual(output, expected, diff)
+                except AssertionError:
+                    diff = ''.join(difflib.unified_diff(
+                        expected.splitlines(True), output.splitlines(True),
+                        expected_path, params['destination_path']))
+                    print('\n%s:' % (self,), file=sys.stderr)
+                    print(diff, file=sys.stderr)
+                    raise
+                # Execute optional function containing extra tests:
+                if '_test_more' in namespace:
+                    namespace['_test_more'](join_path(datadir, 'expected'),
+                                            join_path(datadir, 'output'),
+                                            self, namespace)
 
 
 if __name__ == '__main__':
