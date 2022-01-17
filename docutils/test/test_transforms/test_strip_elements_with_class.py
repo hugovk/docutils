9b0356bcd4a5d99bb6784d895112bbb3be398a58
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2022-01-17 17:27:14 +0000

    Start moving to subTests -- TransformTestCase

--- docutils/test/test_transforms/test_strip_elements_with_class.py
+++ docutils/test/test_transforms/test_strip_elements_with_class.py
@@ -12,22 +12,49 @@ import unittest
 from test import DocutilsTestSupport
 from docutils.parsers.rst import Parser
 from docutils.transforms.universal import StripClassesAndElements
+from docutils import frontend
+from docutils import utils
+from docutils.parsers import rst
+from docutils.transforms import universal
 
-def suite():
+
+class TransformTestCase(DocutilsTestSupport.CustomTestCase):
+
+    """
+    Output checker for the transform.
+
+    Should probably be called TransformOutputChecker, but I can deal with
+    that later when/if someone comes up with a category of transform test
+    cases that have nothing to do with the input and output of the transform.
+    """
+
+    option_parser = frontend.OptionParser(components=(rst.Parser,))
+    settings = option_parser.get_default_values()
+    settings.report_level = 1
+    settings.halt_level = 5
+    settings.debug = False
+    settings.warning_stream = DocutilsTestSupport.DevNull()
+    unknown_reference_resolvers = ()
     parser = Parser()
-    suite_id = DocutilsTestSupport.make_id(__file__)
-    s = unittest.TestSuite()
-    for name, (transforms, cases) in totest.items():
-        for casenum, (case_input, case_expected) in enumerate(cases):
-            s.addTest(
-                DocutilsTestSupport.TransformTestCase("test_transforms",
-                                                      input=case_input, expected=case_expected,
-                                                      id='%s: totest[%r][%s]' % (suite_id, name, casenum),
-                                                      suite_settings={'strip_elements_with_classes': ['spam', 'no-ham'],
-                                                                      'strip_classes': ['spam', 'noise']},
-                                                      transforms=transforms, parser=parser)
-            )
-    return s
+
+    def test_transforms(self):
+        for name, (transforms, cases) in totest.items():
+            for casenum, (case_input, case_expected) in enumerate(cases):
+                with self.subTest(id=f'totest[{name!r}][{casenum}]'):
+                    settings = self.settings.copy()
+                    settings.strip_elements_with_classes = ['spam', 'no-ham']
+                    settings.strip_classes = ['spam', 'noise']
+                    document = utils.new_document('test data', settings)
+                    self.parser.parse(case_input, document)
+                    # Don't do a ``populate_from_components()`` because that would
+                    # enable the Transformer's default transforms.
+                    document.transformer.add_transforms(transforms)
+                    document.transformer.add_transform(universal.TestMessages)
+                    document.transformer.components['writer'] = self
+                    document.transformer.apply_transforms()
+                    output = document.pformat()
+                    DocutilsTestSupport._compare_output(self, case_input, output, case_expected)
+
 
 totest = {}
 
--- docutils/test/test_transforms/test_target_notes.py
+++ docutils/test/test_transforms/test_target_notes.py
@@ -15,22 +15,46 @@ from docutils.transforms.references import PropagateTargets, \
      AnonymousHyperlinks, IndirectHyperlinks, ExternalTargets, \
      InternalTargets, DanglingReferences, Footnotes
 from docutils.parsers.rst import Parser
+from docutils import frontend
+from docutils import utils
+from docutils.parsers import rst
+from docutils.transforms import universal
 
 
-def suite():
+class TransformTestCase(DocutilsTestSupport.CustomTestCase):
+
+    """
+    Output checker for the transform.
+
+    Should probably be called TransformOutputChecker, but I can deal with
+    that later when/if someone comes up with a category of transform test
+    cases that have nothing to do with the input and output of the transform.
+    """
+
+    option_parser = frontend.OptionParser(components=(rst.Parser,))
+    settings = option_parser.get_default_values()
+    settings.report_level = 1
+    settings.halt_level = 5
+    settings.debug = False
+    settings.warning_stream = DocutilsTestSupport.DevNull()
+    unknown_reference_resolvers = ()
     parser = Parser()
-    suite_id = DocutilsTestSupport.make_id(__file__)
-    s = unittest.TestSuite()
-    for name, (transforms, cases) in totest.items():
-        for casenum, (case_input, case_expected) in enumerate(cases):
-            s.addTest(
-                DocutilsTestSupport.TransformTestCase("test_transforms",
-                                                      input=case_input, expected=case_expected,
-                                                      id='%s: totest[%r][%s]' % (suite_id, name, casenum),
-                                                      suite_settings={},
-                                                      transforms=transforms, parser=parser)
-            )
-    return s
+
+    def test_transforms(self):
+        for name, (transforms, cases) in totest.items():
+            for casenum, (case_input, case_expected) in enumerate(cases):
+                with self.subTest(id=f'totest[{name!r}][{casenum}]'):
+                    document = utils.new_document('test data', self.settings.copy())
+                    self.parser.parse(case_input, document)
+                    # Don't do a ``populate_from_components()`` because that would
+                    # enable the Transformer's default transforms.
+                    document.transformer.add_transforms(transforms)
+                    document.transformer.add_transform(universal.TestMessages)
+                    document.transformer.components['writer'] = self
+                    document.transformer.apply_transforms()
+                    output = document.pformat()
+                    DocutilsTestSupport._compare_output(self, case_input, output, case_expected)
+
 
 totest = {}
 
